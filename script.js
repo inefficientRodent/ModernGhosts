044e2713b487d78b9a049d3b969e3f84b707d5a6
Mckx Hamilton <60514699+inefficientRodent@users.noreply.github.com>
2020-08-18 11:46:54 +0100

    Surprise!

--- script.js
+++ script.js
@@ -11,20 +11,17 @@ var gameData = {
   //Clown Purchase Variables
   comedyCost: 20,
   jugglingCost: 125,
-  balancingCost: 999,
+  balancingCost: 500,
+  animalCost: 750,
+  stuntCost: 1500,
+  pretzelCost: 3000,
+  dangerousCost: 
   
   //Building amounts
-  varTent: 0,
   tentCost: 10,
-  
-  varClownvan: 50,
   vanCost:0,
-  
-  varMotel: 0,
-  motelCost:0,
-  
-  varClowndo:0,
-  clowndoCost: 0
+  motelCost:0, 
+  clowndoCost: 0,
 }
 //This should load the game properly now
 var savegame = JSON.parse(localStorage.getItem("ClownCircus"))
@@ -194,3 +191,28 @@ var updateLoop = window.setInterval(function() {
 function debugLodesMone(money) {
   gameData.varCash = money
 }
+
+
+
+
+
+//Confetti explosion from codepen.io user xxrobot (Scott Sasaki)
+$('button').on('click', function(){  
+  function random(max){
+      return Math.random() * (max - 0) + 0;
+  }
+
+  var c = document.createDocumentFragment();
+  for (var i=0; i<100; i++) {
+    var styles = 'transform: translate3d(' + (random(500) - 250) + 'px, ' + (random(200) - 150) + 'px, 0) rotate(' + random(360) + 'deg);\
+                  background: hsla('+random(360)+',100%,50%,1);\
+                  animation: bang 700ms ease-out forwards;\
+                  opacity: 0';
+      
+    var e = document.createElement("i");
+    e.style.cssText = styles.toString();
+    c.appendChild(e);
+}
+// document.body.appendChild(c);
+  $(this).append(c);
+})
